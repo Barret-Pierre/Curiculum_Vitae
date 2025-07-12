94dd204a74be18509a3a818e17f684c1f3a84e1f
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2025-07-13 00:04:55 +0200

    style(css): updating animation

--- index.js
+++ index.js
@@ -3,36 +3,35 @@
 // élements devant s'animer au scroll
 const elementsSlideBottom = document.querySelectorAll(".on-scroll");
 
-// éléments pour l'appartition du menu 
+// éléments pour l'appartition du menu
 const menuNavigation = document.querySelector(".navbar");
 const sectionSoft = document.querySelector(".section_soft");
 
 // ********************************** Evènements **********************************
 
-// Au scroll 
-window.addEventListener('scroll', function (e) {
+// Au scroll
+window.addEventListener("scroll", function (e) {
+  // créer une constante récupérant les propritété de l'objet documentElement la position du haut de la page html et la hauteur de la partie visible de la fenêtre
+  const { scrollTop, clientHeight } = document.documentElement;
 
+  for (let element of elementsSlideBottom) {
+    // donne la distance entre le haut de l'objet et celui du viewport
+    const topElementToTopViewport = element.getBoundingClientRect().top;
+    // getBoundingClientRect retourne un objet contenant les information sur la taille et la position de l'élément par rapport à la zone d'affichage ici on isolera la propriété top
 
-    // créer une constante récupérant les propritété de l'objet documentElement la position du haut de la page html et la hauteur de la partie visible de la fenêtre
-    const { scrollTop, clientHeight } = document.documentElement;
-    console.log(scrollTop, clientHeight);
-
-    for (let element of elementsSlideBottom) {
-
-        // donne la distance entre le haut de l'objet et celui du viewport
-        const topElementToTopViewport = element.getBoundingClientRect().top;
-        // getBoundingClientRect retourne un objet contenant les information sur la taille et la position de l'élément par rapport à la zone d'affichage ici on isolera la propriété top
-
-        if (scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 1) {
-            element.classList.add("active");
-        }
+    if (
+      scrollTop >
+      (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 1
+    ) {
+      element.classList.add("active");
     }
+  }
 
-    const topSectioSoftToTopViewport = sectionSoft.getBoundingClientRect().top;
+  const topSectioSoftToTopViewport = sectionSoft.getBoundingClientRect().top;
 
-    if (scrollTop > (scrollTop + topSectioSoftToTopViewport).toFixed()) {
-        menuNavigation.classList.remove("desactive")
-    } else {
-        menuNavigation.classList.add("desactive");
-    }
-})
+  if (scrollTop > (scrollTop + topSectioSoftToTopViewport).toFixed()) {
+    menuNavigation.classList.remove("desactive");
+  } else {
+    menuNavigation.classList.add("desactive");
+  }
+});
